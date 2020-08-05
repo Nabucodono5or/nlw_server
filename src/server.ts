5510324c247e9d134663c82dc8c39f696b8ae3f0
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-05 12:43:19 -0300

    inserido dependencias para executar um servidor

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+
+app.listen(3333);
+
+app.get("/users", (request, response) => {
+  const users = [
+    {
+      name: "Vinicius",
+      age: 22,
+    },
+    {
+      name: "Jefferson",
+      age: 29,
+    },
+    {
+      name: "Jonathan",
+      age: 29,
+    },
+  ];
+
+  return response.json(users);
+});
