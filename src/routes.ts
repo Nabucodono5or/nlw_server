35c12d8772806d96ef8f5e575afc3a939033e47b
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-08-06 11:15:23 -0300

    inserido dado repetido em users e inserido dado novo em classes

--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,19 @@ const routes = express.Router();
 routes.post("/classes", async (request, response) => {
   const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
 
-  await db('users').insert({
+  const insertedUsersId = await db("users").insert({
     name,
     avatar,
     whatsapp,
     bio,
+  });
+
+  const user_id = insertedUsersId[0];
+
+  await db("classes").insert({
+    subject,
+    cost,
+    user_id,
   });
 
   return response.send();
